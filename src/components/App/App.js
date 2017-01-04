9c3bfc78911717c9d9d9598ef5bbfede9ca0bdb4
Philipp Brumm <philipp@railslove.com>
2017-01-04 13:46:18 +0100

    fix wrong indent

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -39,6 +39,7 @@ class App extends React.Component {
   quickLook(path) {
     electronWindow.previewFile(path)
   }
+
   browseTo(path) {
     getDirectoryContent(path).then(items => {
       if (items.length) {
@@ -136,121 +137,123 @@ class App extends React.Component {
           {({ resolved: paneObjects = this.previousPaneObjects, rejected, pending: loading }) => {
             if (rejected) { throw rejected }
 
-              paneObjects = paneObjects.filter(({ paneObject, searchTerm }) => paneObject || searchTerm)
+            paneObjects = paneObjects.filter(({ paneObject, searchTerm }) => paneObject || searchTerm)
 
-              const activePaneObject = paneObjects[activePaneIndex].paneObject
-              const activePaneResults = paneObjects[activePaneIndex].paneResults
+            const activePaneObject = paneObjects[activePaneIndex].paneObject
+            const activePaneResults = paneObjects[activePaneIndex].paneResults
 
-              let listItemHeight = 50
-              let listHeight = Math.min(activePaneResults.length * listItemHeight, 4.5 * listItemHeight)
+            let listItemHeight = 50
+            let listHeight = Math.min(activePaneResults.length * listItemHeight, 4.5 * listItemHeight)
 
-              if (!loading) {
-                this.previousPaneObjects = paneObjects
-                electronWindow.setSize(paneObjects.length * 200, listHeight + 185)
-              }
+            if (!loading) {
+              this.previousPaneObjects = paneObjects
+              electronWindow.setSize(paneObjects.length * 200, listHeight + 185)
+            }
 
-              return (
-                <Flex grow={1} direction='column'>
+            return (
+              <Flex grow={1} direction='column'>
 
-                  {!loading && activePaneObject && activePaneObject.components && browsingAt !== '/' &&
-                    <KeyHandler keyEventName='keydown' keyValue='ArrowLeft'
-                      onKeyHandle={e => {
-                        e.preventDefault()
-                        this.browseTo(activePaneObject.components.slice(0, -2).join('/'))
-                      }}
+                {!loading && activePaneObject && activePaneObject.components && browsingAt !== '/' &&
+                  <KeyHandler keyEventName='keydown' keyValue='ArrowLeft'
+                    onKeyHandle={e => {
+                      e.preventDefault()
+                      this.browseTo(activePaneObject.components.slice(0, -2).join('/'))
+                    }}
+                  />
+                }
+
+                {!loading && activePaneObject &&
+                  <KeyHandler keyEventName='keydown' keyValue='ArrowRight'
+                    onKeyHandle={e => {
+                      e.preventDefault()
+                      activePaneObject.uti.includes('public.folder')
+                        ? this.browseTo(activePaneObject.path)
+                        : this.quickLook(activePaneObject.path)
+                    }}
+                  />
+                }
+
+                {!loading && activePaneObject &&
+                  <KeyHandler keyEventName='keydown' keyValue=' '
+                    onKeyHandle={e => {
+                      e.preventDefault()
+                      activePaneObject.uti.includes('public.folder')
+                        ? this.browseTo(activePaneObject.path)
+                        : this.quickLook(activePaneObject.path)
+                    }}
+                  />
+                }
+
+                {!loading && activePaneObject &&
+                  <KeyHandler keyEventName='keydown' keyValue='ArrowDown'
+                    onKeyHandle={e => {
+                      e.preventDefault()
+                      this.changePaneResultIndex(
+                        wrap(activePaneResultIndex + 1, activePaneResults.length)
+                      )
+                    }
+                  } />
+                }
+
+                {!loading && activePaneObject &&
+                  <KeyHandler keyEventName='keydown' keyValue='ArrowUp'
+                    onKeyHandle={e => {
+                      e.preventDefault()
+                      this.changePaneResultIndex(
+                        wrap(activePaneResultIndex - 1, activePaneResults.length)
+                      )
+                    }
+                  } />
+                }
+
+                {!loading && activePaneObject &&
+                  <KeyHandler keyEventName='keydown' keyValue='Tab'
+                    onKeyHandle={e => {
+                      e.preventDefault()
+                      this.changeActivePaneIndex(
+                        wrap(activePaneIndex + (e.shiftKey ? -1 : 1), paneObjects.length)
+                      )
+                    }
+                  } />
+                }
+
+                {activeSearchTerm &&
+                  <KeyHandler keyEventName='keydown' keyValue='Backspace'
+                    onKeyHandle={e => {
+                      e.preventDefault()
+                      this.clearSearch()
+                    }} />
+                }
+
+                <Flex className={css.sentence}>
+                  {paneObjects.length !== 1
+                    ? paneObjects.map(({ paneObject = {}, searchTerm }, paneIndex) => (
+                    <Pane
+                      key={`pane-${paneIndex}`}
+                      active={paneIndex === activePaneIndex}
+                      label={paneObject.displayName || paneObject.name}
+                      searchTerm={searchTerm}
+                      icon={paneObject.icon || paneObject.path}
+                      changeActivePaneIndex={() => this.changeActivePaneIndex(paneIndex)}
                     />
-                  }
+                )) : [
+                  <Pane key='directObjectId' active label='Type to search' />,
+                  <Pane key='actionObjectId' />
+                ]}
+                </Flex>
 
-                  {!loading && activePaneObject &&
-                    <KeyHandler keyEventName='keydown' keyValue='ArrowRight'
-                      onKeyHandle={e => {
-                        e.preventDefault()
-                        activePaneObject.uti.includes('public.folder')
-                          ? this.browseTo(activePaneObject.path)
-                          : this.quickLook(activePaneObject.path)
-                      }}
-                    />
-                  }
-
-                  {!loading && activePaneObject &&
-                    <KeyHandler keyEventName='keydown' keyValue=' '
-                      onKeyHandle={e => {
-                        e.preventDefault()
-                        activePaneObject.uti.includes('public.folder')
-                          ? this.browseTo(activePaneObject.path)
-                          : this.quickLook(activePaneObject.path)
-                      }}
-                    />
-                  }
-
-                  {!loading && activePaneObject &&
-                    <KeyHandler keyEventName='keydown' keyValue='ArrowDown'
-                      onKeyHandle={e => {
-                        e.preventDefault()
-                        this.changePaneResultIndex(
-                          wrap(activePaneResultIndex + 1, activePaneResults.length)
-                        )
-                      }
-                    } />
-                  }
-
-                  {!loading && activePaneObject &&
-                    <KeyHandler keyEventName='keydown' keyValue='ArrowUp'
-                      onKeyHandle={e => {
-                        e.preventDefault()
-                        this.changePaneResultIndex(
-                          wrap(activePaneResultIndex - 1, activePaneResults.length)
-                        )
-                      }
-                    } />
-                  }
-
-                  {!loading && activePaneObject &&
-                    <KeyHandler keyEventName='keydown' keyValue='Tab'
-                      onKeyHandle={e => {
-                        e.preventDefault()
-                        this.changeActivePaneIndex(
-                          wrap(activePaneIndex + (e.shiftKey ? -1 : 1), paneObjects.length)
-                        )
-                      }
-                    } />
-                  }
-
-                  {activeSearchTerm &&
-                    <KeyHandler keyEventName='keydown' keyValue='Backspace'
-                      onKeyHandle={e => {
-                        e.preventDefault()
-                        this.clearSearch()
-                      }} />
-                  }
-
-                  <Flex className={css.sentence}>
-                    {paneObjects.length !== 1
-                      ? paneObjects.map(({ paneObject = {}, searchTerm }, paneIndex) => (
-                      <Pane
-                        key={`pane-${paneIndex}`}
-                        active={paneIndex === activePaneIndex}
-                        label={paneObject.displayName || paneObject.name}
-                        searchTerm={searchTerm}
-                        icon={paneObject.icon || paneObject.path}
-                        changeActivePaneIndex={() => this.changeActivePaneIndex(paneIndex)}
-                      />
-                  )) : [<Pane key='directObjectId' active label='Type to search' />, <Pane key='actionObjectId' />]}
-                  </Flex>
-
-                  <Flex grow={1}>
-                    <QTList
-                      loading={loading}
-                      height={listHeight}
-                      listItemHeight={listItemHeight}
-                      items={activePaneResults}
-                      selectedIndex={activePaneResultIndex}
-                      onIndexChange={index => this.changePaneResultIndex(index)}
-                    />
-                  </Flex>
+                <Flex grow={1}>
+                  <QTList
+                    loading={loading}
+                    height={listHeight}
+                    listItemHeight={listItemHeight}
+                    items={activePaneResults}
+                    selectedIndex={activePaneResultIndex}
+                    onIndexChange={index => this.changePaneResultIndex(index)}
+                  />
+                </Flex>
               </Flex>
-              )
-
+            )
           }}
         </Pinky>
       </Flex>
